f7194a253b7c8c60437523638b596a04efd93aa7
Steven <sqxinquan@hotmail.com>
2023-03-21 21:43:48 +0800

    install

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/CreateTableITCase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/CreateTableITCase.java
@@ -15,7 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*
+  install
+ */
 package org.apache.paimon.flink;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
